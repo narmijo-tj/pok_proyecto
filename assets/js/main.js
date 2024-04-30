9c06076127fa559af8d06dd247f5896a4fa5039a
narmijo-tj <toffee.jaguargmail.com>
2024-04-30 17:27:45 -0400

    medidas pokemones corregidas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,73 +1,46 @@
 $(()=> {
-
-    //contiene el pokemon que se está mostrando
     let currentPokemon;
-    //va a contener todos los pokémones que registremos
     let arrayPokemones = [];
-    /* CAPTURAR EL FORMULARIO */
     const formFindPokemon = $("#formFindPokemon");
-    
-    
-    //ENCIENDO EVENTO SUBMIT DEL FORMULARIO
     formFindPokemon.on("submit", function(event){
-        //QUITAMOS EL EVENTO POR DEFECTO DEL FORMULARIO (ACTUALIZAR PÁGINA)
         event.preventDefault();
-
-        /* CAPTURAR ID O NOMBRE DEL POKEMON DESDE EL INPUT DEL FORMULARIO */
         let idOrName = $("#findPokemon").val().toLowerCase();
-
-        let rutaAPI = "https://pokeapi.co/api/v2/pokemon/"+idOrName;
-        
+        let rutaAPI = "https://pokeapi.co/api/v2/pokemon/"+idOrName;    
         fetch(rutaAPI)
         .then(function(response){
             if(response.status == 404){
-                /* SI EL SERVIDOR NOS RESPONDE CON UN 404 (NO ENCONTRADO)
-                GENERAMOS UNA EXCEPCIÓN / ERROR QUE SE CAPTURA EN EL CATCH*/
                 throw new Error("Pokémon no encontrado.");
             }
-            //PERMITE RETORNAR LA DATA QUE DEVUELVE LA API (INFO DE POKÉMONES)
             return response.json();
         })
         .then(function(data){
-            //EN DATA NOS LLEGA LA INFORMACIÓN DEL POKÉMON CONSULTADO
             let habilidades = data.abilities.map(habilidad => {
                 return habilidad.ability.name
             })
-            
             let pokemon = {
                 id: data.id,
                 nombre: data.name,
                 imagen: data.sprites.other.dream_world.front_default,
-                habilidades: habilidades //array que contiene nombre de habilidades
+                habilidades: habilidades 
             }
-
-            //dejamos como currentPokemon al actual pokémon
             currentPokemon = pokemon;
-
             cargarDatosPokemonCard(pokemon);
         })
         .catch(function(error){
-            //CAPTURAMOS ERROR Y LO MOSTRAMOS EN UNA VENTANA EMERGENTE
             console.log(error);
             alert(error);
         })
     })
     function cargarDatosPokemonCard(pokemon){
-        //se reemplaza imagen y alt que tiene el card por la imagen pokemon buscado y nombre
         $("#cardPokemon > img").attr("src", pokemon.imagen);
         $("#cardPokemon > img").attr("alt", pokemon.nombre);
         $("#cardPokemon .card-title").text(pokemon.nombre.toUpperCase());
-        
-        //SE CREA UN ACUMULADOR DE ELEMENTOS LI
         let liHabilidades = "";
-        //RE RECORRE ARRAY DE HABILIDADES PARA CREAR TANTOS LI COMO HABILIDADES TENGA
-        //EL POKÉMON
         pokemon.habilidades.forEach(function(habilidad){
             liHabilidades += `
                 <li>${habilidad}</li>
             `
         })
-        //LE AGREGAMOS LOS ELEMENTOS DE LISTA A LA UL (LISTA)
         $("#listaHabilidades").html(liHabilidades);
     }
 })
@@ -75,7 +48,7 @@ const listaPokemon = document.querySelector("#listaPokemon");
 const botonesHeader = document.querySelectorAll(".btn-header");
 let URL = "https://pokeapi.co/api/v2/pokemon/";
 
-for (let i = 1; i <= 807; i++) {
+for (let i = 1; i <= 1025; i++) {
     fetch(URL + i)
         .then(function(response){
             if(response.status == 404){
@@ -85,18 +58,14 @@ for (let i = 1; i <= 807; i++) {
         .then(data => mostrarPokemon(data))
 }
 function mostrarPokemon(poke) {
-
     let tipos = poke.types.map((type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`);
     tipos = tipos.join('');
-
     let pokeId = poke.id.toString();
     if (pokeId.length === 1) {
         pokeId = "00" + pokeId;
     } else if (pokeId.length === 2) {
         pokeId = "0" + pokeId;
     }
-
-
     const div = document.createElement("div");
     div.classList.add("pokemon");
     div.innerHTML = `
@@ -113,22 +82,20 @@ function mostrarPokemon(poke) {
                 ${tipos}
             </div>
             <div class="pokemon-stats">
-                <p class="stat">${poke.height}m</p>
-                <p class="stat">${poke.weight}kg</p>
+                <p class="stat">${poke.height/10} m</p>
+                <p class="stat">${poke.weight/10} kg</p>
             </div>
         </div>
     `;
     listaPokemon.append(div);
 }
-
 botonesHeader.forEach(boton => boton.addEventListener("click", (event) => {
     const botonId = event.currentTarget.id;
     listaPokemon.innerHTML = "";
-    for (let i = 1; i <= 807; i++) {
+    for (let i = 1; i <= 1025; i++) {
         fetch(URL + i)
             .then((response) => response.json())
             .then(data => {
-
                 if(botonId === "ver-todos") {
                     mostrarPokemon(data);
                 } else {
